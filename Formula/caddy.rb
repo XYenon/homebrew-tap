75100eeba627b5ee4c46926c6a7698e5b58a21da
XYenon <i@xyenon.bid>
2021-02-06 21:15:33 +0800

    feat: add cloudflare

---
+++ Formula/caddy.rb
@@ -0,0 +1,88 @@
+class Caddy < Formula
+  desc "Powerful, enterprise-ready, open source web server with automatic HTTPS"
+  homepage "https://caddyserver.com/"
+  url "https://github.com/caddyserver/caddy/archive/v2.3.0.tar.gz"
+  sha256 "4688b122ac05be39622aa81324d1635f1642e4a66d731e82d210aef78cf2766a"
+  license "Apache-2.0"
+  head "https://github.com/caddyserver/caddy.git"
+
+  option "with-cloudflare", "Compile with dns.providers.cloudflare"
+
+  depends_on "go" => :build
+
+  resource "xcaddy" do
+    url "https://github.com/caddyserver/xcaddy/archive/v0.1.7.tar.gz"
+    sha256 "9915970e69c07324f4d032741741516523147b6250e1426b16e6b794d4a56f05"
+  end
+
+  def with_list
+    list = []
+    if build.with? "cloudflare"
+      list.push "--with", "github.com/caddy-dns/cloudflare"
+    end
+  end
+
+  def install
+    revision = build.head? ? version.commit : "v#{version}"
+
+    resource("xcaddy").stage do
+      system "go", "run", "cmd/xcaddy/main.go", "build", revision, "--output", bin/"caddy", *with_list
+    end
+  end
+
+  plist_options manual: "caddy run --config #{HOMEBREW_PREFIX}/etc/Caddyfile"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <true/>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/caddy</string>
+            <string>run</string>
+            <string>--config</string>
+            <string>#{etc}/Caddyfile</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/caddy.log</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/caddy.log</string>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    port1 = free_port
+    port2 = free_port
+
+    (testpath/"Caddyfile").write <<~EOS
+      {
+        admin 127.0.0.1:#{port1}
+      }
+
+      http://127.0.0.1:#{port2} {
+        respond "Hello, Caddy!"
+      }
+    EOS
+
+    fork do
+      exec bin/"caddy", "run", "--config", testpath/"Caddyfile"
+    end
+    sleep 2
+
+    assert_match "\":#{port2}\"",
+      shell_output("curl -s http://127.0.0.1:#{port1}/config/apps/http/servers/srv0/listen/0")
+    assert_match "Hello, Caddy!", shell_output("curl -s http://127.0.0.1:#{port2}")
+
+    assert_match version.to_s, shell_output("#{bin}/caddy version")
+  end
+end
